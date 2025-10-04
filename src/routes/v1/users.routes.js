d16f6b303add76e937d1c80135483cd061ae30db
valentina <valentinacorrea0117@gmail.com>
2025-10-04 02:13:52 -0500

    Implementación de rutas GET de users (lista e individual)

--- src/routes/v1/users.routes.js
+++ src/routes/v1/users.routes.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-// aquí iremos agregando endpoints para users
+// Estado en memoria (simulación)
+let users = [
+  {
+    id: "b42f53fa-7b30-4b91-8d36-dc1c6ef27611",
+    name: "Carlos Navia",
+    email: "carlos@example.com",
+    role: "user",
+    createdAt: "2025-09-12T12:00:00Z"
+  }
+];
+
+// GET /api/v1/users  -> lista (sin filtros aún)
+router.get("/", (req, res) => {
+  res.status(200).json(users);
+});
+
+// GET /api/v1/users/:id  -> individual
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find(u => u.id === id);
+  if (!user) return res.status(404).json({ error: "Usuario no encontrado" });
+  res.status(200).json(user);
+});
 
 module.exports = router;
 
